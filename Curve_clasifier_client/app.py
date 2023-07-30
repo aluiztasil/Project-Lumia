2c2f67ab759c975819d905527945883203d9eb33
aluiztasil <aluiztasil@yahoo.com.br>
2023-07-30 02:03:21 -0300

    -- interesting upgrade trial

--- Curve_clasifier_client/app.py
+++ Curve_clasifier_client/app.py
@@ -6,6 +6,22 @@ import pandas as pd
 import os
 import json
 import numpy as np
+import random
+
+def generate_curve_points(curve_name, data):
+    # Extract metadata
+    metadata = data[curve_name]['curve_metadata']
+    
+    # Extract 'LOW', 'HIGH', and 'NPOINTS' values
+    low = metadata['LOW']
+    high = metadata['HIGH']
+    npoints = metadata['NPOINTS']
+    
+    # Generate a list with evenly distributed points
+    points = np.linspace(low, high, npoints).tolist()
+    
+    return points
+
 
 app = Flask(__name__, static_url_path='/Curve_clasifier_client/static')
 app.config['WTF_CSRF_ENABLED'] = False
@@ -26,69 +42,72 @@ def intro(): #add a status page to get to know the status of the corrected datas
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     global user_responses
+    global sampled_curve_names
     if 'username' not in session or 'session_size' not in session:
         return redirect(url_for('intro'))
         # Read data from .csv or .json
     file_path = 'Curve_clasifier_client/data/input.json'
     extension = '.json'
-    if extension == '.csv':
-        df = pd.read_csv('data/input.csv')
-    elif extension == '.json':
+    # if extension == '.csv':
+    #     df = pd.read_csv('data/input.csv')
+    if extension == '.json':
         with open(file_path) as f:
             data = json.load(f)
-    statement = 'Presta?'
-    # Extract sub-item
-    sub_item_data = np.array(data['curve_data']).T
+    statement = 'Presta?' # what to measure, what statement is should be evaluated?
+  
+    # Sample curves given the session size
+    session_size = min(50, len(data))  # Ensure session size is not larger than total number of curves
+    sampled_curve_names = random.sample(list(data.keys()), session_size)
+
+    # Collect curve data for sampled curves
+    sub_item_data = np.array([data[curve_name]['curve_data'] for curve_name in sampled_curve_names]).T
 
     # Create data frame
-    columns = data['curve_metadata']['SAMPLE_ID']
-    df = pd.DataFrame(sub_item_data, columns = columns)
-    #df = pd.read_json('data/input.json',) #add headers as the curve ids stored
-    temperature = [i for i in range(25,375)]
+    df = pd.DataFrame(sub_item_data, columns=sampled_curve_names)
+
+    # Iterate over sampled curves and process their metadata
+    for curve_name in sampled_curve_names: # instead of iterating here address the item via (user_response varivable)
+        # Extract metadata
+        metadata = data[curve_name]['curve_metadata']
+        
+        # Extract values
+        high_value = metadata['HIGH']
+        low_value = metadata['LOW']
+        npoints_value = metadata['NPOINTS']
+        rate_value = metadata['RATE']
+        an_temp_value = metadata['AN_TEMP']
+        an_time_value = metadata['AN_TIME']
+        
+        # Generate curve points
+        temperature = generate_curve_points(curve_name, data)
+        plt.figure(figsize=(10, 6))
+        plt.plot(temperature, df[curve_name])
+        plt.xlabel('Temperature')
+        plt.ylabel('Luminescent')
+        plt.title('Temperature vs Luminescent')
+        plt.grid(True)
+        plt.savefig('Curve_clasifier_client/static/image.png', format='png') # --  up to this part itworks, the page doesnt render
 
 
-    # # Determine which curves have been measured in previous sessions
-    # try:
-    #     df_out = pd.read_csv('output.csv')
-    #     measured_curves = df_out['curve_id'].unique()
-    # except FileNotFoundError:
-    #     measured_curves = []
+ 
 
-    # # Filter out the measured curves
-    # df = df[~df['curve_id'].isin(measured_curves)]
+
+
 
     if df.empty:
         return render_template('end.html')
 
-    # Select a curve to measure
-
-        #curve_id =  df.columns.iloc[0] #select the firs unmeasured curve
-    if len(user_responses) == 0:
-        curve_id =  list(df.columns)[0]
-    if len(user_responses) > 0:
-        curve_id =  list(df.columns)[len(user_responses)]  
-
-    #it
-    # Plot chart
-    plt.figure(figsize=(10, 6))
-    plt.plot(temperature, df[curve_id])
-    plt.xlabel('Temperature')
-    plt.ylabel('Luminescent')
-    plt.title('Temperature vs Luminescent')
-    plt.grid(True)
-    plt.savefig('Curve_clasifier_client/static/image.png', format='png') # --  up to this part itworks, the page doesnt render
-
-    #curve_id = 'your_curve_id'  # Replace with code to get curve id
+ 
     if request.method == 'POST':
         choice = request.form.get('choice')
         user_responses.append({
             "measured_attribute": choice,
             "username": session['username'],
             "session_size": session['session_size'],
-            "curve_id": curve_id
+            "curve_id": curve_name
         })
         #fix output file generation
-        if len(user_responses) % 5 == 0:  # if there are five new responses
+        if len(user_responses) % 5 == 0:  # save file only in the end of the session (?)
             try:
                 # try to read the existing file
                 df_out = pd.read_csv("Curve_clasifier_client/data/output.csv")
@@ -107,8 +126,9 @@ def home():
             return redirect(url_for('end'))
 
 
-    return render_template('index.html', curve_id=curve_id, statement=statement)
-
+    return render_template('index.html', curve_id=curve_name, statement=statement,
+                           high_value=high_value, low_value=low_value, npoints_value=npoints_value,
+                           rate_value=rate_value, an_temp_value=an_temp_value, an_time_value=an_time_value)
 
 @app.route('/end', methods=['GET'])
 def end():
