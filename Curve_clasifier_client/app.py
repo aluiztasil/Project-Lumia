3c5ab4b520458730911bf6936beba3f951e1fba7
aluiztasil <aluiztasil@yahoo.com.br>
2023-08-03 12:33:55 -0300

    -- updating for it to work LC

--- Curve_clasifier_client/app.py
+++ Curve_clasifier_client/app.py
@@ -46,7 +46,7 @@ def home():
     if 'username' not in session or 'session_size' not in session:
         return redirect(url_for('intro'))
         # Read data from .csv or .json
-    file_path = 'Curve_clasifier_client/data/input.json'
+    file_path = '/Users/andretavares/Documents/GitHub/Project-Lumia-v2/Curve_clasifier_client/data/database_TL_other.json'
     extension = '.json'
     # if extension == '.csv':
     #     df = pd.read_csv('data/input.csv')
@@ -56,22 +56,14 @@ def home():
     statement = 'Presta?' # what to measure, what statement is should be evaluated? #where should this information be caught from.
     attribute_name = ''
     # Sample curves given the session size
-    session_size = min(50, int(session['session_size']))  # Ensure session size is not larger than total number of curves
-    sampled_curve_names = random.sample(list(data.keys()), session_size)
+    # session_size = max(0, min(len(data.keys()), int(session['session_size'])))
+    sampled_curve_names = random.sample(list(data.keys()), int(session['session_size'])) #session_size)
 
-    # Collect curve data for sampled curves
-    sub_item_data = np.array([data[curve_name]['curve_data'] for curve_name in sampled_curve_names]).T
-
-    # Create data frame
-    df = pd.DataFrame(sub_item_data, columns=sampled_curve_names)
-
-    # Iterate over sampled curves and process their metadata
-    #for curve_name in sampled_curve_names: 
-    # # instead of iterating here address the item via (user_response varivable)
+ 
     curve_name = sampled_curve_names[len(user_responses)]
     # Extract metadata
     metadata = data[curve_name]['curve_metadata']
-    
+    curve_data = data[curve_name]['curve_data']
     # Extract values
     high_value = metadata['HIGH']
     low_value = metadata['LOW']
@@ -83,12 +75,16 @@ def home():
     # Generate curve points
     temperature = generate_curve_points(curve_name, data)
     plt.figure(figsize=(10, 6))
-    plt.plot(temperature, df[curve_name])
+    plt.plot(temperature, curve_data)
     plt.xlabel('Temperature')
     plt.ylabel('Luminescent')
     plt.title('Temperature vs Luminescent')
     plt.grid(True)
-    plt.savefig('Curve_clasifier_client/static/image.png', format='png') # --  up to this part itworks, the page doesnt render
+    plt.show()
+    plt.savefig('/Users/andretavares/Documents/GitHub/Project-Lumia-v2/Curve_clasifier_client/static/image.png', format='png') # --  up to this part itworks, the page doesnt render
+
+    #Calculate session size
+    countdown = 'Current Session: ' + str(len(user_responses)) + ' / ' +str(session['session_size'])
 
 
  
@@ -96,8 +92,8 @@ def home():
 
 
 
-    if df.empty:
-        return render_template('end.html')
+    # if df.empty:
+    #     return render_template('end.html')
 
  
     if request.method == 'POST':
@@ -109,18 +105,20 @@ def home():
             "curve_id": curve_name
         })
         #fix output file generation
-        if len(user_responses) % int(session['session_size']):  # save file only in the end of the session (?)
+        if len(user_responses) % int(session['session_size'])==0:  # save file only in the end of the session (?)
             try:
-                # try to read the existing file
-                df_out = pd.read_csv("Curve_clasifier_client/data/output.csv")
-                # append new responses to the existing DataFrame
-                new_df = pd.DataFrame(user_responses)
-                df_out = df_out.append(new_df, ignore_index=True)
-            except FileNotFoundError:
-                # if the file doesn't exist, create a new DataFrame
+            #     # try to read the existing file
+            #     df_out = pd.read_csv("Curve_clasifier_client/data/output.csv")
+            #     # append new responses to the existing DataFrame
+            #     new_df = pd.DataFrame(user_responses)
+            #     df_out = df_out.append(new_df, ignore_index=True)
+            # except FileNotFoundError:
+            #     # if the file doesn't exist, create a new DataFrame
                 df_out = pd.DataFrame(user_responses)
+            except:
+                pass
             # save DataFrame to .csv
-            df_out.to_csv("Curve_clasifier_client/data/output.csv", index=False)
+            df_out.to_csv("Curve_clasifier_client/data/output_{}.csv".format(session['username']), index=False)
             # reset the list after saving
             #user_responses = []
         if len(user_responses) >= int(session['session_size']):
@@ -130,7 +128,7 @@ def home():
 
     return render_template('index.html', curve_id=curve_name, statement=statement,
                            high_value=high_value, low_value=low_value, npoints_value=npoints_value,
-                           rate_value=rate_value, an_temp_value=an_temp_value, an_time_value=an_time_value)
+                           rate_value=rate_value, an_temp_value=an_temp_value, an_time_value=an_time_value, countdown=countdown)
 
 @app.route('/end', methods=['GET'])
 def end():
